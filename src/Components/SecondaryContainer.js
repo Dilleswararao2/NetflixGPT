0962acbb08c972df20531036ce05851cda097e44
Kadambala Dilleswara Rao <150668082+Dilleswararao2@users.noreply.github.com>
2025-06-30 10:48:15 +0530

    Update SecondaryContainer.js
    fixed a bug in secondary container (right side white background)

--- src/Components/SecondaryContainer.js
+++ src/Components/SecondaryContainer.js
@@ -5,7 +5,7 @@ const SecondaryContainer = () => {
   const movies = useSelector((store) => store.movies);
   return (
     movies.nowPlayingMovies && (
-      <div className="bg-black">
+      <div className="bg-black w-screen">
         <div className="mt-0 md:-mt-80 pl-2 md:pl-12 relative z-20">
           <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
           <MovieList title={"Popular"} movies={movies.popularMovies} />
